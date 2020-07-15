13bd4ad558ca18c94f1d2ce28f4479e54f81b4b0
andregraciano16 <andregraciano16@gmail.com>
2020-07-15 00:39:14 -0300

    deletando transacação

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,8 +1,8 @@
-import { Router } from 'express';
+import { Router, json } from 'express';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
@@ -21,11 +21,13 @@ transactionsRouter.post('/', async (request, response) => {
     category,
   });
   return response.json(transaction);
-
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const deleteTransaction = new DeleteTransactionService();
+  await deleteTransaction.execute(id);
+  return response.json({ ok: true });
 });
 
 transactionsRouter.post('/import', async (request, response) => {
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,17 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import TransactionRepository from '../repositories/TransactionsRepository';
+import AppError from '../errors/AppError';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+    const transaction = await transactionRepository.findOne({ where: { id } });
+    console.log(transaction);
+    if (!transaction) {
+      throw new AppError('Transacion does not exist', 400);
+    }
+    console.log(id);
+    await transactionRepository.delete(id);
   }
 }
 
