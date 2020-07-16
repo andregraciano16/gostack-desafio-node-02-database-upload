e66f04a9038d27bd07c965bece4d817d8813342a
andregraciano16 <andregraciano16@gmail.com>
2020-07-15 23:56:34 -0300

    criando metodo de listar

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -10,9 +10,30 @@ interface Balance {
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
-  // public async getBalance(): Promise<Balance> {
-  //   return balance;
-  // }
+  public async getBalance(): Promise<Balance> {
+    const { income, outcome } = (await this.find()).reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    const total = income - outcome;
+    return { income, outcome, total };
+  }
 }
 
 export default TransactionsRepository;
